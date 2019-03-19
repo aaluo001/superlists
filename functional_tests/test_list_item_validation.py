f977093ab5beadf8a2ce16216556a9eefd337dee
aaluo001 <aaluo008@163.com>
2019-03-19 23:01:07 +0800

    Add jquery, qunit tests, lists.js with keypress listeners

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -57,6 +57,10 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('2: 泡杯茶')
 
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
+
     def test_cannot_add_duplicate_items(self):
         # 新建一个清单
         self.browser.get(self.live_server_url)
@@ -72,8 +76,34 @@ class ItemValidationTest(FunctionalTest):
 
         # 于是，得到一条错误消息
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             '您已经有一条同样的待办事项！'
         ))
 
+
+    def test_error_messages_are_cleared_on_input(self):
+        # 新建一个清单
+        self.browser.get(self.live_server_url)
+        input_box = self.get_item_input_box()
+        input_box.send_keys('买一盒牛奶')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: 买一盒牛奶')
+
+        # 输入一个重复的待办事项
+        input_box = self.get_item_input_box()
+        input_box.send_keys('买一盒牛奶')
+        input_box.send_keys(Keys.ENTER)
+
+        # 于是，得到一条错误消息
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # 再输入内容，错误消息消失
+        input_box = self.get_item_input_box()
+        input_box.send_keys('a')
+
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
 
---
+++ lists/static/lists.js
@@ -0,0 +1,8 @@
+
+window.superlists = {};
+window.superlists.initialize = function() {
+    $('input[name="text"]').on('keypress', function() {
+        $(".has-error").hide();
+    });
+};
+
