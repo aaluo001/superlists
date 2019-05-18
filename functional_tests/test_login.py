87f30684596bdd36ccaaf057634119becd3784db
aaluo001 <aaluo008@163.com>
2019-05-18 23:20:46 +0800

    Finished send login email test and login test

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -18,7 +18,7 @@ from .base import FunctionalTest
 
 
 class LoginTest(FunctionalTest):
-    ''' 发送登录验证邮件和登录测试
+    ''' 登录测试
     '''
     def wait_for_email(self, test_email):
         if (self.staging_tests):
@@ -46,53 +46,15 @@ class LoginTest(FunctionalTest):
                 inbox.quit()
             
         else:
-            email = mail.outbox[0]
-            #print('mail outbox length: {}'.format(len(mail.outbox)))
+            # 第二次发送邮件时，len(mail.outbox) == 2
+            #self.assertEqual(len(mail.outbox), 1)
+            email = mail.outbox[-1]
             self.assertIn(test_email, email.to)
             #self.assertIn('我们给您发送了一条登录验证的链接', email.subject)
             return email.body
 
 
     def test_001(self):
-        ''' 未输入邮箱地址就发送邮件
-        '''
-        # 访问首页
-        # 未输入邮箱地址就发送邮件
-        self.browser.get(self.live_server_url)
-        input_email = self.browser.find_element_by_name('email')
-        input_email.send_keys(Keys.ENTER)
-
-        # 浏览器截获了请求，页面不会被加载
-        self.wait_for(lambda:
-            self.browser.find_element_by_css_selector('#id_text:invalid')
-        )
-
-
-    def test_002(self):
-        ''' 发送错误邮箱地址，会得到发送邮件失败的提示
-            而不会出现Server Error (500)
-        '''
-        # 只能在过渡网站中测试
-        if (not self.staging_tests): return
-        
-        # 访问首页
-        # 在导航栏的登录区域输入错误的邮箱地址
-        self.browser.get(self.live_server_url)
-        input_email = self.browser.find_element_by_name('email')
-        input_email.send_keys('error_email_tests@163.com')
-        input_email.send_keys(Keys.ENTER)
-        
-        # 检测到“邮件发送失败”的消息
-        self.wait_for(lambda: self.assertIn(
-            '邮件发送失败',
-            self.browser.find_element_by_tag_name('body').text
-        ))
-
-        # 没有检测到“ServerError(500)”
-        self.assertNotIn('Server Error (500)', self.browser.page_source)
-
-
-    def test_003(self):
         ''' 输入邮箱地址发送邮件，然后取得登录验证的链接并成功登录
         '''
         if (self.staging_tests):
@@ -133,7 +95,7 @@ class LoginTest(FunctionalTest):
         self.wait_to_be_logged_out(email=test_email)
 
 
-    def test_004(self):
+    def test_002(self):
         ''' 使用错误的链接无法完成登录，并得到登录失败的提示
         '''
         # 使用错误的链接访问网站
@@ -149,7 +111,7 @@ class LoginTest(FunctionalTest):
         self.browser.find_element_by_name('email')
 
 
-    def test_005(self):
+    def test_003(self):
         ''' 对同一邮箱地址发送了两次登录验证的邮件
             那么，第一次得到的登录验证邮件中的链接无法完成登录
             第二次得到的则可以
@@ -180,7 +142,6 @@ class LoginTest(FunctionalTest):
 
  
         # 第二次发送登录验证邮件
-        #self.browser.get(self.live_server_url)
         input_email = self.browser.find_element_by_name('email')
         input_email.send_keys(test_email)
         input_email.send_keys(Keys.ENTER)
---
+++ functional_tests/test_send_login_email_validation.py
@@ -0,0 +1,53 @@
+#!python
+# coding: gbk
+#------------------------------
+# functional_tests.test_send_login_email_validation
+#------------------------------
+# Author: TangJianwei
+# Create: 2019-05-18
+#------------------------------
+from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
+
+
+class SendLoginEmailValidationTest(FunctionalTest):
+    ''' 发送登录邮件验证测试
+    '''
+    def test_001(self):
+        ''' 未输入邮箱地址就发送邮件
+        '''
+        # 访问首页
+        # 未输入邮箱地址就发送邮件
+        self.browser.get(self.live_server_url)
+        input_email = self.browser.find_element_by_name('email')
+        input_email.send_keys(Keys.ENTER)
+
+        # 浏览器截获了请求，页面不会被加载
+        self.wait_for(lambda:
+            self.browser.find_element_by_css_selector('#id_text:invalid')
+        )
+
+
+    def test_002(self):
+        ''' 发送错误邮箱地址，会得到发送邮件失败的提示
+            而不会出现Server Error (500)
+        '''
+        # 只能在过渡网站中测试
+        if (not self.staging_tests): return
+        
+        # 访问首页
+        # 在导航栏的登录区域输入错误的邮箱地址
+        self.browser.get(self.live_server_url)
+        input_email = self.browser.find_element_by_name('email')
+        input_email.send_keys('error_email_tests@163.com')
+        input_email.send_keys(Keys.ENTER)
+        
+        # 检测到“邮件发送失败”的消息
+        self.wait_for(lambda: self.assertIn(
+            '邮件发送失败',
+            self.browser.find_element_by_tag_name('body').text
+        ))
+
+        # 没有检测到“ServerError(500)”
+        self.assertNotIn('Server Error (500)', self.browser.page_source)
+
