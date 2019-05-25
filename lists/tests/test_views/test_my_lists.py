98d0a4f571ab31a874e9a93fcefb299424d4e5d0
aaluo001 <aaluo008@163.com>
2019-05-25 22:30:32 +0800

    Finished test_my_lists

--- lists/tests/test_views/test_my_lists.py
+++ lists/tests/test_views/test_my_lists.py
@@ -10,20 +10,56 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-from lists.models import List
+from lists.models import List, Item
 
 
 class MyListsTest(TestCase):
-
-    def test_my_lists_url_renders_my_lists_template(self):
-        User.objects.create(email='abc@163.com')
-        response = self.client.get('/lists/users/abc@163.com/')
-        self.assertTemplateUsed(response, 'my_lists.html')
-
-
-    def test_passes_correct_owner_to_template(self):
-        User.objects.create(email='wrong@163.com')
+    ''' 我的清单测试
+    '''
+    def test_001(self):
+        ''' 我的清单显示页面
+        '''
         user_object = User.objects.create(email='abc@163.com')
-        response = self.client.get('/lists/users/abc@163.com/')
-        self.assertEqual(response.context['owner'], user_object)
+        self.client.force_login(user_object)
+        response = self.client.get('/lists/')
+        self.assertTemplateUsed(response, 'lists/my_lists.html')
+
+
+    def test_002(self):
+        ''' 我的清单上下文
+        '''
+        user_object = User.objects.create(email='abc@163.com')
+        list_object_1 = List.objects.create(owner=user_object)
+        Item.objects.create(list=list_object_1, text='New item 1')
+        list_object_2 = List.objects.create(owner=user_object)
+        Item.objects.create(list=list_object_2, text='New item 2')
+        
+        other_list_object = List.objects.create()
+        Item.objects.create(list=other_list_object, text='Other item')
+
+        self.client.force_login(user_object)
+        response = self.client.get('/lists/')
+        
+        list_set = response.context['list_set']
+        self.assertEqual(len(list_set), 2)
+        self.assertIn(list_object_1, list_set)
+        self.assertIn(list_object_2, list_set)
+        self.assertNotIn(other_list_object, list_set)
+
+    
+    def test_003(self):
+        ''' 没有清单时不会报错
+        '''
+        user_object = User.objects.create(email='abc@163.com')
+        self.client.force_login(user_object)
+        response = self.client.get('/lists/')
+        self.assertEqual(len(user_object.list_set.all()), 0)
+        self.assertEqual(len(response.context['list_set']), 0)
+        
+        
+    def test_004(self):
+        ''' 匿名用户直接跳转到首页
+        '''
+        response = self.client.get('/lists/')
+        self.assertRedirects(response, '/')
 
