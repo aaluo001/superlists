05c4a77d70da289862342b4dfde2fc185dafae63
aaluo001 <aaluo008@163.com>
2019-05-27 22:50:56 +0800

    finished functional_tests.test_my_lists

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -6,31 +6,29 @@
 # Author: TangJianwei
 # Create: 2019-04-04
 #------------------------------
+import time
 from django.conf import settings
 from django.contrib.sessions.backends.db import SessionStore
-from django.contrib.auth import (
-    get_user_model,
-    BACKEND_SESSION_KEY, SESSION_KEY,
-)
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
 
 
-User = get_user_model()
-
-
 class MyListsTest(FunctionalTest):
-
+    ''' 我的清单测试
+    '''
     def create_pre_authenticated_session(self, email):
         if (self.staging_tests):
             session_key = create_session_on_server(email)
         else:
             session_key = create_pre_authenticated_session(email)
         
-        ## 为了设定cookie，我们要先访问网站
-        ## 而404页面加载最快
+        # 为了设定Cookie，我们要先访问网站
+        # 而404页面加载最快
         self.browser.get(self.live_server_url + '/404_no_such_url/')
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
@@ -39,50 +37,129 @@ class MyListsTest(FunctionalTest):
         ))
 
 
-    def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        ## 测试已经是登录用户了
+    def test_001(self):
+        ''' 登录用户可以新建清单，然后在我的清单页面显示
+        '''
+        # 创建登录用户
         self.create_pre_authenticated_session('abc@163.com')
         
-        # 访问首页，新建一个清单
+        # 新建清单
         self.browser.get(self.live_server_url)
         self.add_list_item('Reticulate splines')
         self.add_list_item('Immanentize eschaton')
-        first_list_url = self.browser.current_url
-        
-        # 出现"待办事项清单"的链接
-        self.browser.find_element_by_link_text("待办事项清单").click()
-        
-        # 待办事项清单页面有刚刚新建的清单
-        # 而且清单根据第一个待办事项命名
+        list_url_1 = self.browser.current_url
+
+        # 点击"我的清单"链接
+        self.browser.find_element_by_link_text('待办事项').click()
+        self.browser.find_element_by_link_text('我的清单').click()
+
+        # 巨幕显示"我的清单"
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('div.text-center > h1').text,
+            '我的清单'
+        ))
+
+        # 我的清单页面出现新建的清单
+        # 而清单名字是第一个待办事项的内容
+        self.wait_for(lambda:
+            self.browser.find_element_by_css_selector('table#id_my_lists_table')
+        )
         self.wait_for(lambda:
             self.browser.find_element_by_link_text('Reticulate splines')
         )
+
+        # 进入清单页面，该页面的链接和新建清单的链接一致
         self.browser.find_element_by_link_text('Reticulate splines').click()
         self.wait_for(lambda:
-            self.assertEqual(self.browser.current_url, first_list_url)
+            self.assertEqual(self.browser.current_url, list_url_1)
         )
+
         
-        # 再建一个待办事项清单
-        self.browser.get(self.live_server_url)
+        # --- 再新建一个待办事项清单 ---
+
+        # 点击"新建清单"链接
+        self.browser.find_element_by_link_text('待办事项').click()
+        self.browser.find_element_by_link_text('新建清单').click()
+        
+        # 新建清单
         self.add_list_item('Click cows')
-        second_list_url = self.browser.current_url
-        
-        # 在"待办事项清单"里面可以看见这个新建的清单
-        self.browser.find_element_by_link_text("待办事项清单").click()
+        list_url_2 = self.browser.current_url
+
+        # 点击"我的清单"链接
+        self.browser.find_element_by_link_text('待办事项').click()
+        self.browser.find_element_by_link_text('我的清单').click()
+
+        # 我的清单页面出现新建的清单
+        # 而清单名字是第一个待办事项的内容
         self.wait_for(lambda:
             self.browser.find_element_by_link_text('Click cows')
         )
+        
+        # 进入清单页面，该页面的链接和新建清单的链接一致
         self.browser.find_element_by_link_text('Click cows').click()
         self.wait_for(lambda:
-            self.assertEqual(self.browser.current_url, second_list_url)
+            self.assertEqual(self.browser.current_url, list_url_2)
         )
 
-        # 退出后，"待办事项清单"链接不见了
+        # 退出后"我的清单"链接不见了
         self.browser.find_element_by_link_text("退出").click()
-        self.wait_for(lambda:
-            self.assertEqual(
-                self.browser.find_elements_by_link_text("待办事项清单"),
-                []
-            )
-        )
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_elements_by_link_text("我的清单"),
+            []
+        ))
+
+        
+    def test_002(self):
+        ''' 登录用户没有创建清单时，我的清单页面没有清单显示
+        '''
+        # 创建登录用户
+        self.create_pre_authenticated_session('abc@163.com')
+        self.browser.get(self.live_server_url)
+
+        # 点击"我的清单"链接
+        self.browser.find_element_by_link_text('待办事项').click()
+        self.browser.find_element_by_link_text('我的清单').click()
+
+        # 巨幕显示"我的清单"
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('div.text-center > h1').text,
+            '我的清单'
+        ))
+        
+        # 没有清单显示
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_elements_by_css_selector('table#id_my_lists_table'),
+            []
+        ))
+        
+        # 显示没有清单的消息
+        self.wait_for(lambda: self.assertIn(
+            '您还没有自己的清单',
+            self.browser.find_element_by_tag_name('body').text
+        ))
+
+    
+    def test_003(self):
+        ''' 匿名用户无法显示我的清单
+        '''
+        # 匿名用户新建清单
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Work at office')
+        self.add_list_item('Have a dinner')
+        
+        # 没有找到"待办事项"菜单
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_elements_by_link_text('待办事项'),
+            []
+        ))
+
+        # 使用URL地址强行进入"我的清单"
+        self.browser.get(self.live_server_url + '/lists/')
+
+        # 页面跳转到首页
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('div.text-center > h1').text,
+            '新建清单'
+        ))
+        self.assertEqual(self.browser.current_url, self.live_server_url + '/')
 
