96bb720ca2a3cd82145e83afd7d44cc783592692
aaluo001 <aaluo008@163.com>
2019-05-29 21:10:01 +0800

    modified lists.test_forms and lists.test_models

---
+++ lists/tests/test_forms/test_item_form.py
@@ -0,0 +1,50 @@
+#!python
+# coding: gbk
+#------------------------------
+# lists.tests.test_forms.test_item_form
+#------------------------------
+# Author: TangJianwei
+# Create: 2019-03-10
+#------------------------------
+from django.test import TestCase
+from bs4 import BeautifulSoup
+
+from lists.models import List, Item
+from lists.forms  import ItemForm
+
+
+class ItemFormTest(TestCase):
+    ''' 新建清单的待办事项表单测试
+    '''
+    def test_001(self):
+        ''' 待办事项输入框及其属性
+        '''
+        soup = BeautifulSoup(ItemForm().as_p(), 'html.parser')
+        #print(soup)
+        item_text = soup.find('input', {'name': 'text'})
+        self.assertEqual(item_text['type'], 'text')
+        self.assertEqual(item_text['placeholder'], '输入待办事项')
+        self.assertEqual(item_text['maxlength'], '32')
+        self.assertEqual(item_text['required'], '')
+        self.assertEqual(item_text['class'], ['form-control', ])
+
+
+    def test_002(self):
+        ''' 不能提交空的待办事项
+        '''
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], ['待办事项不能为空！', ])
+
+        
+    def test_003(self):
+        ''' 将表单内容保存到数据库
+        '''
+        form = ItemForm(data={'text': 'do me'})
+        list_object = List.objects.create()
+        item_object = form.save(for_list=list_object)
+        
+        self.assertEqual(item_object, Item.objects.first())
+        self.assertEqual(item_object.list, list_object)
+        self.assertEqual(item_object.text, 'do me')
+
--- lists/tests/test_models/test_item_model.py
+++ lists/tests/test_models/test_item_model.py
@@ -1,7 +1,7 @@
 #!python
 # coding: gbk
 #------------------------------
-# lists.tests.test_models
+# lists.tests.test_models.test_item_model
 #------------------------------
 # Author: TangJianwei
 # Create: 2019-02-25
@@ -66,48 +66,4 @@ class ItemModelTest(TestCase):
         
         item_object = Item(list=list_object_2, text='do me')
         item_object.full_clean()    ## 不会出现异常
-
-
-class ListModelTest(TestCase):
-    ''' 清单模型测试
-    '''
-    def test_001(self):
-        ''' 取得清单的URL链接
-        '''
-        list_object = List.objects.create()
-        self.assertEqual( \
-            list_object.get_absolute_url(), \
-            '/lists/{}/'.format(list_object.id) \
-        )
-
-
-    def test_002(self):
-        ''' 可以指定清单的拥有者
-        '''
-        user_object = User.objects.create(email='abc@163.com')
-        list_object = List.objects.create(owner=user_object)
-        self.assertIn(list_object, user_object.list_set.all())
-
-
-    def test_003(self):
-        ''' 可以不指定清单的拥有者
-        '''
-        # 新建一条清单数据，清单的拥有者是NULL
-        List.objects.create()
-        self.assertIsNone(List.objects.first().owner)
-        
-        # 下面会出现异常，因为owner字段没有指定blank=True
-        with self.assertRaises(ValidationError):
-            list_object = List()
-            list_object.full_clean()
-            #list_object.save()
-
-
-    def test_004(self):
-        ''' 清单的标题是第一条待办事项
-        '''
-        list_object = List.objects.create()
-        Item.objects.create(list=list_object, text='first item')
-        Item.objects.create(list=list_object, text='second item')
-        self.assertEqual(list_object.name, 'first item')
 
